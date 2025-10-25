c8ebbd26585651284b403c9c01a62b90b6bede6f
mkanapelka <maksimel12work@gmail.com>
2025-10-25 14:31:09 +0200

    Add nodeport for producer

--- k8s-producer/src/main/kotlin/com/example/k8s_producer/api/AppController.kt
+++ k8s-producer/src/main/kotlin/com/example/k8s_producer/api/AppController.kt
@@ -3,6 +3,7 @@ package com.example.k8s_producer.api
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -14,5 +15,10 @@ class AppController(
     @GetMapping("/test")
     fun test() {
         logger.info("PRODUCER app is working")
+    }
+
+    @GetMapping("/ping")
+    fun ping(@RequestParam name: String) {
+        logger.info("PRODUCER is called by {}", name)
     }
 }
